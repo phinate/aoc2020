571a98c1c3849ca3b85d0d0297d2b0af87e216dd
Nathan Simpson <email.nsimpson@gmail.com>
2020-12-18 20:35:05 +0100

    move to scripts

--- day4/day4.py
+++ day4/day4.py
@@ -1,57 +1,61 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-passports = []
-with open('input', 'r') as file:
-    p_dict = {}
-    for line in file.readlines() + [""]:
-        #print(type(line),p_dict)
-        if line.strip():
-            for pair in line.split():
-                key, value = pair.split(':')
-                p_dict[key] = value
-        else:
-            passports.append(p_dict)
-            p_dict = {}
-
-
+from typing import Dict
+from typing import List
 from typing import NamedTuple
+from typing import Union
 
 
 class Passport(NamedTuple):
-    byr: int # (Birth Year)
-    iyr: int # (Issue Year)
-    eyr: int # (Expiration Year)
-    hgt: str # (Height)
-    hcl: str # (Hair Color)
-    ecl: str # (Eye Color)
-    pid: int # (Passport ID)
-    #cid: int # (Country ID)
+    byr: int  # (Birth Year)
+    iyr: int  # (Issue Year)
+    eyr: int  # (Expiration Year)
+    hgt: str  # (Height)
+    hcl: str  # (Hair Color)
+    ecl: str  # (Eye Color)
+    pid: int  # (Passport ID)
+    # cid: int # (Country ID)
 
 
-
-pass_dict = {'ecl': 'brn',
-             'pid': 9337568136,
-             'eyr': 2026,
-             'hcl': '#6b5442',
-             'hgt': '69cm',
-             #'iyr' :2019,
-             'byr': 2025}
-    
-valid = 0
-for p_dict in passports:
-    p_dict.pop('cid', None)
-
+def check_passport(passport: Dict[str, str]) -> bool:
+    for field, val in passport.items():
+        temp_pass: Dict[str, Union[str, int]] = {}
+        newval: Union[int, str]
+        if field in ['byr', 'iyr', 'eyr', 'pid']:
+            try:
+                newval = int(val)
+            except Exception as ex:
+                print(f'invalid passport: {ex}')
+                return False
+        else:
+            newval = val
+        temp_pass[field] = newval
     try:
-        Passport(**p_dict)
-        valid += 1
-    except Exception as e:
-        print(e)
-        #print('bad passport!')
-        #raise err
+        x = Passport(**temp_pass)  # type: ignore # noqa
+    except Exception as ex:
+        print(f'invalid passport: {ex}')
+        return False
+    return True
 
 
+def num_valid(passports: List[Dict[str, str]]) -> int:
+    valid = 0
+    for p_dict in passports:
+        p_dict.pop('cid', None)
+        valid += check_passport(p_dict)
+    return valid
 
 
+if __name__ == "__main__":
 
+    passports: List[Dict[str, str]] = []
+    with open('input') as file:
+        p_dict: Dict[str, str] = {}
+        for line in file.readlines() + [""]:
+            # print(type(line),p_dict)
+            if line.strip():
+                for pair in line.split():
+                    key, value = pair.split(':')
+                    p_dict[key] = value
+            else:
+                passports.append(p_dict)
+                p_dict = {}
+    print(num_valid(passports))
